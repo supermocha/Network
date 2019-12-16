de5fea3af1cd5b185c48fe2de2d085c49c81805a
Cheryl Chen <cheryl.chen@thinkpower-info.com>
2019-12-16 15:50:18 +0800

    Initial Commit

---
+++ Network2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Network2
+//
+//  Created by Cheryl Chen on 2019/12/16.
+//  Copyright © 2019 Cheryl Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
